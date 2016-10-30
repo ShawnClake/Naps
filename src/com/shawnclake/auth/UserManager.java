d357b0a208bf67b88569ce2c4f5c0f0f2afb1406
ShawnClake <shawn.clake@gmail.com>
2016-10-30 12:53:26 -0600

    Moved to the proper package folder. Created skeleton for UserManager

---
+++ src/com/shawnclake/auth/UserManager.java
@@ -0,0 +1,25 @@
+package com.shawnclake.auth;
+
+public class UserManager {
+
+    public void createUser()
+    {
+
+    }
+
+    public User readUser()
+    {
+
+    }
+
+    public void updateUser()
+    {
+
+    }
+
+    public void deleteUser()
+    {
+
+    }
+
+}
