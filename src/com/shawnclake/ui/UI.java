7879b901185d9322632ba5fcb56c538e8b2feb65
herasymj <jennifer.herasymuik@gmail.com>
2016-10-31 20:42:18 -0600

    Added the main UI for the program
    Start of main, utilizes the auth package for now.

---
+++ src/com/shawnclake/ui/UI.java
@@ -0,0 +1,118 @@
+package com.shawnclake.ui;
+
+import com.shawnclake.auth;
+import com.shawnclake.graph;
+
+import java.util.Scanner;
+/*******************************************************************************
+ * ENSE 374 Project - Naps
+ * 
+ * Scrum cycle 1
+ * 
+ * Description: Our team is building an app that helps the users take control of
+ * 	their sleep cycle. They start by creating an account to save all their settings
+ * 	and data to, as well as personalize their experience. This app allows the users
+ *  to integrate their fitbit into the app and actually monitor their quality of 
+ *  sleep. This data is saved to their account and the user can view this data in
+ *  a graph. It also allows the user to set bedtime reminders as well as personalized
+ *  wake up alarms. Other features will be added as the project progresses.
+ * 
+ * Team Members: Shawn Clake, Uys Kriek, Sam Dietrich, Jennifer Herasymuik
+ * 
+ * Date: October 31st, 2016
+ * Author: Jennifer Herasymuik
+ * ******************************************************************************/
+public class UI {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		String choice; // will be used to take in the user inputs throughout the program
+		
+		///////////////////////////////////Login Screen/////////////////////////////////////
+		//sign in or register here
+		//this calls the auth classes
+		System.out.println("Welcome to NAPS...");
+		System.out.println("Would you like to login or register for a new account?");
+		choice = in.nextLine();
+		choice = choice.toLowerCase();
+		
+		//create the current sessions user
+		User current = new User();
+		
+		if(choice.equals("login")){
+			boolean valid = true;
+			do{
+			//get users username and password
+			System.out.println("LOGIN");
+			System.out.print("Username: ");
+			String userName = in.nextLine();
+			System.out.print("Password: ");
+			String pass = in.nextLine();
+			
+			current = current.login(userName, pass);
+			
+			//check if valid user
+			if(current.username.equals("")){
+				System.out.println("Login was incorrect.");
+				System.out.println("Would you like to try again, or continue as guest?");
+				System.out.println("Please enter either 'try again' or 'continue'");
+				String continueGuest = in.nextLine();
+				continueGuest = continueGuest.toLowerCase();
+				if (continueGuest.equals("continue")){
+					valid = false;
+					break;
+				}
+				else{
+					//retry
+					valid = false;
+				}
+			}
+			}while(!valid);
+			
+		}
+		if (choice.equals("register")){
+			//get user information the call register function (username email password);
+			System.out.println("REGISTER");
+			System.out.print("Please enter a username: ");
+			String username = in.nextLine();
+			System.out.print("Please enter a password: ");
+			String password = in.nextLine();
+			System.out.print("Please verify password: ");
+			String passwordCheck = in.nextLine();
+			while (!(password.equals(passwordCheck))){
+				System.out.println("Passwords do not match. Please re-enter password.");
+				System.out.print("Please enter password: ");
+				password = in.nextLine();
+				System.out.print("Verify password: ");
+				passwordCheck = in.nextLine();
+			}
+			System.out.print("Enter an e-mail address: ");
+			String email = in.nextLine();
+			
+			current = current.register(username, password, email);
+			//new user has been registered and is the current session user
+		}
+		
+		
+		////////////////MAIN LOOP///////////////////////////////////////////////////////
+		///////Loop runs for the duration of the program
+		///////It will allow the user to use the reminders, fitbit, and displaying results
+		System.out.println("NAPS");
+		System.out.println("Select one of the following features:");
+		//list features
+		//fitbit class
+		//reminder class
+		//graph class
+		
+		
+		in.close();
+		
+	}
+
+}
+
+
+
+
+
