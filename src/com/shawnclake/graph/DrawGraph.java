b13d3d97866dba45c88814f2a7606640cd9c7173
herasymj <jennifer.herasymuik@gmail.com>
2016-10-31 19:11:18 -0600

    Add files via upload
    Actual graphing classes, stub coding functions not finished.

---
+++ src/com/shawnclake/graph/DrawGraph.java
@@ -0,0 +1,26 @@
+package project;
+
+/*****************************************************************************
+ * DrawGraph Class
+ * 
+ * Description: This class is responsible for displaying the graph to the user.
+ * 
+ * Date: October 31st
+ * Author: Jennifer Herasymuik
+ *****************************************************************************/
+
+public class DrawGraph {
+	
+	private Graph graph;
+	
+	//this function uses the graph member variable and draws out the graph
+	//for now the graph will be viewed as a table of data points
+	public void drawGraph(){
+		
+	}
+	
+	//this functions takes in a graph, then sets the graph member variable to this graph
+	public void setGraph(Graph g){
+		
+	}
+}
---
+++ src/com/shawnclake/graph/Graph.java
@@ -0,0 +1,46 @@
+package project;
+
+/*****************************************************************************
+ * Graph Class
+ * 
+ * Description: This class contains all of the graph information such as the
+ * 	axis labels and values to go into graph.
+ * 
+ * Date: October 31st
+ * Author: Jennifer Herasymuik
+ *****************************************************************************/
+
+public class Graph {
+
+	private String xlabels[];
+	private String ylabels[];
+	private int data[][];
+
+	public Graph(){
+		//set xlabels
+		//set ylabels
+		//set data
+		
+	}
+	
+	//sets the x-axis label
+	public void setXLables(String x[]){
+		
+	}
+	
+	//sets the y-axis label
+	public void setYLables(String y[]){
+		
+	}
+	
+	//takes in an array of data points and sets the values of the data member variable
+	public void setData(int d[][]){
+		
+	}
+	
+	
+	public void push(int xVal, int yVal){
+		
+	}
+	
+}
