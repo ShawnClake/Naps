077fe17299faac9890b899370cfdb34ba72229a5
herasymj <jennifer.herasymuik@gmail.com>
2016-11-02 02:40:06 -0600

    Updated set graph function

--- src/com/shawnclake/graph/DrawGraph.java
+++ src/com/shawnclake/graph/DrawGraph.java
@@ -16,16 +16,13 @@ public class DrawGraph {
 	//this function uses the graph member variable and draws out the graph
 	//for now the graph will be viewed as a table of data points
 	public void drawGraph(){
-		System.out.println("(" + graph.xlabel[0] + ", " + graph.ylabel[0]+ ")");
-		for(int i = 0; graph.data[i][0] != null; i++){
-			System.out.println("(" + graph.data[i][0] + ", " + graph.data[i][1] + ")");
-		}
+		
 	}
 	
 	//this functions takes in a graph, then sets the graph member variable to this graph
 	public void setGraph(Graph g){
-		graph.setXLabels(g.xlabels[]);
-		graph.setYLabels(g.ylabels[]);
-		graph.setData(g.data[][]);
+		graph.setXLabels(g.getXLabels());
+		graph.setYLabels(g.getYLabels());
+		graph.setData(g.getData());
 	}
 }
